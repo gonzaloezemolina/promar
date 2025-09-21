f4dd65b08b5ecc48dbfeddcca56ee121a1e1c861
gonzalomolina <gonzaloezemolina@gmail.com>
2025-09-21 18:40:30 -0300

    last version

--- js/index.js
+++ js/index.js
@@ -10,3 +10,27 @@
       carrusel.style.animationPlayState = 'running';
     });
   });
+
+
+  gsap.registerPlugin(MotionPathPlugin);
+  const obtainAirplane = document.getElementById("airplane");
+  obtainAirplane.style.rotate = '40deg'
+
+  gsap.set("#airplane", {
+  motionPath: {
+    path: "#flightPath",
+    align: "#flightPath",
+    alignOrigin: [0.5, 0.5],
+  }
+});
+
+  gsap.to("#airplane", {
+    duration: 5,
+    ease: "power1.inOut",
+    rotate: 45,
+    motionPath: {
+      path: "#flightPath",
+      align: "#flightPath",
+      alignOrigin: [0.5, 0.5]
+    }
+  });
